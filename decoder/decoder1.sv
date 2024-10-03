9cb1f139f78186f8d677050613ea8628b6d59699
Shrikrishna10 <panditshrikrishna10@gmail.com>
2024-10-03 18:50:20 +0530

    output files also

--- decoder/decoder1.sv
+++ decoder/decoder1.sv
@@ -1,2 +1,5 @@
-module decoder(input [31:0]din, output [31:0] dout);
-
+module decoder1(input [4:0]din, output [31:0] dout);
+always_comb begin
+dout = 32'b1 << din;
+end
+endmodule
---
+++ mux/mux_2_tb.sv
@@ -0,0 +1,2 @@
+module mux_2_tb(input [31:0] d0, d1, input s, output y);
+
